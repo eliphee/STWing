7634161b769fae2d8de65d96f16ef70b0a6f66bd
askat86 <askat@hab-verschlafen.de>
2015-08-26 14:46:10 +0200

    DB Class ergänzung

---
+++ Assets/GameData/DataBaseHelper/CaptainUpgradeCollection.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System;
+
+[XmlRoot("captainUpgrades")]
+public class CaptainUpgradeCollection {
+	
+	[XmlArray("captainUpgrade")]
+	[XmlArrayItem("captainSpecial")]
+	public List<CaptainUpgradeClass> captainUpgradeClass = new List<CaptainUpgradeClass>();
+	
+	public static CaptainUpgradeCollection Load(string path)
+	{
+		XmlSerializer serializer = new XmlSerializer(typeof(CaptainUpgradeCollection));
+		
+		XmlReader reader = XmlReader.Create(path);
+		
+		reader.Read();
+		
+		CaptainUpgradeCollection captainUpgrade = (CaptainUpgradeCollection)serializer.Deserialize(reader);
+		
+		reader.Close();
+		
+		return captainUpgrade;
+	}
+}
---
+++ Assets/GameData/DataBaseHelper/XML Interpreter/CaptainUpgradeClass.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class CaptainUpgradeClass {
+
+	[XmlAttribute("id")]
+	public float id;
+	
+	[XmlElement("name")]
+	public string name;
+	
+	[XmlElement("faction")]
+	public string faction;
+	
+	[XmlElement("unique")]
+	public bool unique;
+	
+	[XmlElement("shipUnique")]
+	public bool shipUnique;
+	
+	[XmlElement("squadronPoints")]
+	public float squadronPoints;
+	
+	[XmlElement("restriction")]
+	public string restriction;
+	
+	[XmlElement("restrictionPenalty")]
+	public float restrictionPenalty;
+}
