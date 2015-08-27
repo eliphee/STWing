9cf5bcbf4dc0aacbb2b286652125fe915d7e5771
eliphee <ma-koenig@gmx.net>
2015-08-27 17:55:25 +0200

    DrawNavigation working
    Display the movement pattern of the selected ship in the Maneuver-box of
    the GUI.
    Needs some rework with exact position and coloration of the arrows.
    Attached a demo on mouseDown on Nebula.

--- Assets/GameData/Code/GUI/DrawNavigation.cs
+++ Assets/GameData/Code/GUI/DrawNavigation.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class DrawNavigation : MonoBehaviour {
 
@@ -46,20 +47,183 @@ public class DrawNavigation : MonoBehaviour {
 	public GameObject arrowLeftTurn;
 	public GameObject arrowRightBank;
 	public GameObject arrowRightTurn;
-	
+	public GameObject arrowFullAstern;
+
 	// Use this for initialization
 	void Start ()
 	{
 		panelMovement = GameObject.Find ("Panel Movement");
-		GameObject newArrow = GameObject.Instantiate (arrowUp);
-		newArrow.transform.SetParent (panelMovement.transform);
-		RectTransform newArrow_rt = newArrow.GetComponent<RectTransform>();
-		newArrow_rt.anchoredPosition = new Vector2 (-75.5f, 68.7f);
-		Debug.Log (newArrow.transform.parent.name + " -- " + newArrow.transform.position.x + "," + newArrow.transform.position.y + "," + newArrow.transform.position.z);
+//		GameObject newArrow = GameObject.Instantiate (arrowUp);
+//		newArrow.transform.SetParent (panelMovement.transform);
+//		RectTransform newArrow_rt = newArrow.GetComponent<RectTransform>();
+//		newArrow_rt.anchoredPosition = new Vector2 (23.3f, 68.7f);
+//		Debug.Log (newArrow.transform.parent.name + " -- " + newArrow.transform.position.x + "," + newArrow.transform.position.y + "," + newArrow.transform.position.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	void OnMouseDown()
+	{
+
+//		<pattern id="8">
+//			<maneuver speed="1" bearing="Straight" difficulty="green" />
+//			<maneuver speed="1" bearing="LeftBank" difficulty="green" />
+//			<maneuver speed="1" bearing="RightBank" difficulty="green" />
+//			<maneuver speed="2" bearing="Straight" difficulty="green" />
+//			<maneuver speed="2" bearing="LeftBank" difficulty="white" />
+//			<maneuver speed="2" bearing="LeftTurn" difficulty="white" />
+//			<maneuver speed="2" bearing="RightBank" difficulty="white" />
+//			<maneuver speed="2" bearing="RightTurn" difficulty="white" />
+//			<maneuver speed="3" bearing="Straight" difficulty="white" />
+//			<maneuver speed="3" bearing="LeftBank" difficulty="white" />
+//			<maneuver speed="3" bearing="LeftTurn" difficulty="red" />
+//			<maneuver speed="3" bearing="RightBank" difficulty="white" />
+//			<maneuver speed="3" bearing="RightTurn" difficulty="red" />
+//			<maneuver speed="4" bearing="Straight" difficulty="white" />
+//			<maneuver speed="-1" bearing="FullAstern" difficulty="red" />
+//		</pattern>
+
+
+		Ship.Maneuver[] debugManeuvers = new Ship.Maneuver[]
+		{
+			new Ship.Maneuver(1, "Straight", "green"),
+			new Ship.Maneuver(1, "LeftBank", "green"),
+			new Ship.Maneuver(1, "RightBank", "green"),
+			new Ship.Maneuver(2, "Straight", "green"),
+			new Ship.Maneuver(2, "LeftBank", "white"),
+			new Ship.Maneuver(2, "LeftTurn", "white"),
+			new Ship.Maneuver(2, "RightBank", "white"),
+			new Ship.Maneuver(2, "RightTurn", "white"),
+			new Ship.Maneuver(3, "Straight", "white"),
+			new Ship.Maneuver(3, "LeftBank", "white"),
+			new Ship.Maneuver(3, "LeftTurn", "red"),
+			new Ship.Maneuver(3, "RightBank", "white"),
+			new Ship.Maneuver(3, "RightTurn", "red"),
+			new Ship.Maneuver(4, "Straight", "white"),
+			new Ship.Maneuver(-1, "FullAstern", "red")
+		};
+
+		// Calculate BaseLine row
+		int baseLine = GetBaseLine (debugManeuvers);
+
+		foreach (Ship.Maneuver m in debugManeuvers)
+		{
+			DrawArrow (m.speed, baseLine, m.bearing, m.difficulty);
+		}
+
+	}
+
+	/// <summary>
+	/// Gets the base line.
+	/// </summary>
+	/// <returns>The base line.</returns>
+	/// <param name="maneuvers">Maneuvers.</param>
+	private int GetBaseLine(Ship.Maneuver[] maneuvers)
+	{
+		int baseLine = 5; //Standard baseline
+		int maxSpeed = 1;
+
+		foreach (Ship.Maneuver m in maneuvers)
+		{
+			if (m.speed > maxSpeed)
+			{
+				maxSpeed = m.speed;
+			}
+		}
+
+		if (maxSpeed > 5)
+		{
+			baseLine = maxSpeed;
+		}
+
+		return baseLine;
+	}
+
+	/// <summary>
+	/// Draws the arrow.
+	/// </summary>
+	/// <param name="speed">Speed.</param>
+	/// <param name="baseLine">Base line.</param>
+	/// <param name="bearing">Bearing.</param>
+	/// <param name="difficulty">Difficulty.</param>
+	private void DrawArrow(int speed, int baseLine, string bearing, string difficulty)
+	{
+		// Create new GameObject "Arrow"
+		GameObject newArrow;
+		float posX, posY;
+		int row, col;
+
+		// Load Arrow-image and determine column to insert into based on bearing
+		switch (bearing)
+		{
+			case "Straight":
+				newArrow = GameObject.Instantiate (arrowUp);
+				col = 4;
+				break;
+			case "ComeAbout":
+				newArrow = GameObject.Instantiate (arrowComeabout);
+				col = 7;
+				break;
+			case "LeftBank":
+				newArrow = GameObject.Instantiate (arrowLeftBank);
+				col = 3;
+				break;
+			case "LeftTurn":
+				newArrow = GameObject.Instantiate (arrowLeftTurn);
+				col = 2;
+				break;
+			case "RightBank":
+				newArrow = GameObject.Instantiate (arrowRightBank);
+				col = 5;
+				break;
+			case "RightTurn":
+				newArrow = GameObject.Instantiate (arrowRightTurn);
+				col = 6;
+				break;
+			case "FullAstern":
+				newArrow = GameObject.Instantiate (arrowFullAstern);
+				col = 4;
+				break;
+			default:
+				return;
+		}
+
+		// Determine coords based on input row and column
+		if (speed > 0)
+		{
+			row = baseLine + 1 - speed;
+		} 
+		else
+		{
+			row = baseLine - speed;
+		}
+		
+		posX = -100.2f + col * 24.7f;
+		posY = 93.4f - row * 24.7f;
+
+		// Colorize arrow based on difficulty
+//		switch (difficulty)
+//		{
+//			case "green":
+//				newArrow.GetComponent<Image>().color = Color.green;
+//				break;
+//			case "white":
+//				newArrow.GetComponent<Image>().color = Color.white;
+//				break;
+//			case "red":
+//				newArrow.GetComponent<Image>().color = Color.red;
+//				break;
+//			default:
+//				return;
+//		}
+
+		// Attach new GameObject to Panel
+		newArrow.transform.SetParent (panelMovement.transform);
+		RectTransform newArrow_rt = newArrow.GetComponent<RectTransform>();
+		newArrow_rt.anchoredPosition = new Vector2 (posX, posY);
+	}
+
 }
