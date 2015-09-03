be8ad4afaf1ab659771406be70894ecdbe7bebf7
askat86 <askat@hab-verschlafen.de>
2015-09-03 20:53:09 +0200

    added Main Menu with basic functionality

---
+++ Assets/Scripts/MenuLogic/MainButtons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainButtons : MonoBehaviour {
+
+	// Start FleetManager
+	public void startFM () 
+	{
+		Application.LoadLevel("FleetManager");
+	}
+
+	// Start Main
+	public void startGame () 
+	{
+		Application.LoadLevel("Main");
+	}
+}
